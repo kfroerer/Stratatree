fc96442125bac00b22ff86819f027071971b85fd
victoria517 <victoria.517.ramirez@gmail.com>
2019-02-04 21:40:48 -0600

    fixed eslint errors

--- models/strategy.js
+++ models/strategy.js
@@ -1,18 +1,18 @@
-module.exports = function (sequelize, DataTypes) {
-    var Strategy = sequelize.define("Strategy", {
-        strategy: {
-            type: DataTypes.TEXT,
-        },
-        body: {
-            type: DataTypes.TEXT
-        }});
+module.exports = function(sequelize, DataTypes) {
+  var Strategy = sequelize.define("Strategy", {
+    strategy: {
+      type: DataTypes.TEXT,
+    },
+    body: {
+      type: DataTypes.TEXT
+    }});
 
-        Strategy.associate = function (models) {
-            Strategy.belongsTo(models.Goal, {
-                foreignKey: {
-                    name: "uid",
-                  }
-            })
-        }
-    }
-    return Strategy;
+  Strategy.associate = function(models) {
+    Strategy.belongsTo(models.Goal, {
+      foreignKey: {
+        name: "uid"
+      }
+    });
+  };
+};
+return Strategy;
