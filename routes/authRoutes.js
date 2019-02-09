63885fed3cb259456cdda9c50083850b517212ab
kjojones <43149867+kfroerer@users.noreply.github.com>
2019-02-09 10:04:23 -0600

    fixed travis spacing issues

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,40 +1,38 @@
-var passport = require('passport');
-var jwt = require('jsonwebtoken');
+var passport = require("passport");
+var jwt = require("jsonwebtoken");
 
 module.exports = function(app) {
-    app.post('/api/auth', function(request, response) {
-        passport.authenticate(
-            'local', 
-            {session: false}, 
-            function(error, user, info) {
-                if (error || !user) {
-                    return response.status(403).json({
-                        message: 'Unable to Authorize',
-                        user   : user,
-                        error  : error,
-                        info: info
-                    });
-                }
-               request.login(user, {session: false}, function(error) {
-                   if (error) {
-                       response.send(error);
-                   }
-                   var sanitizedUser = {
-                       id: user.id,
-                       username: user.username,
-                       email: user.email
-                   };
-
-                   // generate a signed son web token with the contents of user object and return it in the response
-                   var token = jwt.sign(sanitizedUser, 'your_jwt_secret');
-                   response.json(
-                       {
-                           user: sanitizedUser,
-                           token: token
-                       }
-                   );
-                });
+  app.post("/api/auth", function(request, response) {
+    passport.authenticate("local", { session: false }, function(
+      error,
+      user,
+      info) {
+        if (error || !user){
+            return response.status(403).json({
+              message: "Unable to Authorize",
+              user   : user,
+              error  : error,
+              info: info
+              });
             }
-        )(request, response);
+            request.login(user, { session: false }, function(error) {
+              if (error) {
+                  response.send(error);
+              }
+              var sanitizedUser = {
+                id: user.id,
+                  username: user.username,
+                  email: user.email
+              };
+
+                // generate a signed son web token with the contents of user object and return it in the response
+              var token = jwt.sign(sanitizedUser, "your_jwt_secret");
+                response.json({
+                    user: sanitizedUser,
+                    token: token
+                  }
+              );
+            });
+        })(request, response);
     });
 };
